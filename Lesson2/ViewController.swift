bbccc42c57cd4a1670cac03281ae4da0a019e60e
Yaroslav.Pashkevich <yaroslav3091989@gmail.com>
2021-07-21 19:12:50 +0300

    Initial Commit

---
+++ Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by  Yaroslav on 21.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
